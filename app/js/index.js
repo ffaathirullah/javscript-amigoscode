3b45ac7b0d350dd72647f16db5ca6af588eeef88
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-07-25 22:59:13 +0700

    array

--- app/js/index.js
+++ app/js/index.js
@@ -4,18 +4,30 @@ require("styles/main.scss");
 import $ from "jquery";
 import { log, logTitle } from "logger";
 /* your imports */
-logTitle("object dan variabel");
+logTitle("fundamentals");
 /* coding examples */
 
-var name = {
-  name: "Fachrul",
-  age: 21,
-  address: {
-    ttl: "bandung",
-    ttl2: "cijeruk",
-  },
-};
+// var name = {
+//   name: "Fachrul",
+//   age: 21,
+//   address: {
+//     ttl: "bandung",
+//     ttl2: "cijeruk",
+//   },
+// };
 
-log(name.name);
-log(name.address.ttl);
-log(name.name + " type = " + typeof name.name);
+// log(name.name);
+// log(name.address.ttl);
+// log(name.name + " type = " + typeof name.name);
+
+var names = ["alex", "agus", "alex2"];
+log(names[1]);
+log(names.length);
+
+for (var n of names) {
+  log(n);
+}
+
+names.forEach(function (n, index) {
+  log(index + " - " + n);
+});
