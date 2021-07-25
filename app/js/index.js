d8e22f80a472eede96d5ac38ab3da7a005f0efca
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-07-25 22:44:17 +0700

    object dan variabel

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,21 @@
 /* styling */
-require('styles/main.scss');
+require("styles/main.scss");
 /* js */
-import $ from 'jquery';
-import { log, logTitle } from 'logger';
+import $ from "jquery";
+import { log, logTitle } from "logger";
 /* your imports */
-logTitle('Title')
+logTitle("object dan variabel");
 /* coding examples */
+
+var name = {
+  name: "Fachrul",
+  age: 21,
+  address: {
+    ttl: "bandung",
+    ttl2: "cijeruk",
+  },
+};
+
+log(name.name);
+log(name.address.ttl);
+log(name.name + " type = " + typeof name.name);
